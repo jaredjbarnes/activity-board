55edac92ead9a5b3c7eb31c22cc36fcf75e4b242
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2023-07-06 13:46:28 -0600

    Updated standard.

--- src/models/event_template_types/i_standard_event_type.ts
+++ src/models/event_template_types/i_standard_event_type.ts
@@ -2,6 +2,6 @@ import { EventTypeName } from "src/models/event_type_name.ts";
 
 export interface IStandardEventType {
   name: EventTypeName.Standard;
-  startDate: number; // In milliseconds from Unix Epoch
-  duration: number; // In milliseconds
+  startOn: number; // In milliseconds from Unix Epoch
+  endOn: number; // In milliseconds from Unix Epoch
 }
--- src/models/i_event_template.ts
+++ src/models/i_event_template.ts
@@ -1,11 +1,9 @@
 import { IEventType } from "src/models/i_event_type.ts";
 
-// Base event template interface
 export interface IEventTemplate {
   id: string;
   title: string;
-  description?: string;
-  location?: string;
+  notes: string | null;
   eventType: IEventType;
 }
 
